c7ef472f441264a8e63103059d4b899b2907e4e4
Kåre Sylow Pedersen <ksp@tradeshift.com>
2015-06-16 14:40:57 +0200

    database connection works

--- src/main/java/rest/MessageResource.java
+++ src/main/java/rest/MessageResource.java
@@ -8,23 +8,33 @@ import org.springframework.stereotype.Component;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
-/**
- * Created by ksp on 08/06/15.
- */
 
 @Component
-@Path("names/{name}")
+@Path("/messages")
 public class MessageResource {
 
     @Autowired
     private MessageService messageService;
 
+    @Autowired
+    private MessageDAO messageDAO;
+
+
     @POST
+    @Path("/names/{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public MessageService getIt(@PathParam("name") String name) {
+    public MessageService postMessage(@PathParam("name") String name) {
         messageService.setMessage("hello " + name);
         return messageService;
+    }
+
+    @GET
+    @Path("/recent")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Message> recentMessenges(){
+        return messageDAO.getLatestMesseges();
     }
 
 
--- src/main/java/rest/MessageService.java
+++ src/main/java/rest/MessageService.java
@@ -1,7 +1,13 @@
 package rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by ksp on 08/06/15.
@@ -16,13 +22,14 @@ public class MessageService {
     }
 
     public Message getMessage(){
-        return this.message;
+        return message;
     }
 }
 
 @Component
 class Message{
     private String content;
+    private Timestamp timestamp;
 
     public void setContent(String content){
         this.content = content;
@@ -30,5 +37,15 @@ class Message{
 
     public String getContent(){
         return content;
+    }
+
+    public void setTimestamp(Timestamp timestamp){
+        this.timestamp = timestamp;
+    }
+
+    public String getTimestamp() throws Exception{
+        SimpleDateFormat dateFormatGmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+
+        return dateFormatGmt.format(timestamp);
     }
 }
